3a8832baed4538e16e1dc9a66a0a05c467b6fe37
Ybalrid (Arthur Brainville) <ybalrid@ybalrid.info>
2023-06-01 15:51:43 +0200

    Use standard format_string, always.

--- header/ytl_fmt.hpp
+++ header/ytl_fmt.hpp
@@ -6,13 +6,6 @@ namespace ytl
 {
 	using print_function = void(*)(const char*);
 
-	//Alias the template string type for compatibility bodges
-#if _MSC_VER >= 1930 //TODO FIXFIX after visual studio actually expose format_string properly
-#define __YTL_FORMAT_STRING  std::_Fmt_string
-#else
-#define __YTL_FORMAT_STRING  std::format_string<T>; 
-#endif
-
 	class fmt_sink
 	{
 		print_function print = nullptr;
@@ -23,12 +16,10 @@ namespace ytl
 		}
 
 		template <typename ... Args>
-		void operator()(__YTL_FORMAT_STRING<Args...> fmt, Args ... args)
+		void operator()(std::format_string<Args...> fmt, Args ... args)
 		{
 			assert(nullptr != print);
 			print(std::format(fmt, std::forward<Args>(args) ...).c_str());
 		}
 	};
-
-#undef __YTL_FORMAT_STRING
 }

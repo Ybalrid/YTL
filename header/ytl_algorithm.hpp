fb8f81a8e44e4675e352dbbf2f401c9f36137a65
Ybalrid (Arthur Brainville) <ybalrid@ybalrid.info>
2023-06-01 11:17:13 +0200

    tiny cleanup

--- header/ytl_algorithm.hpp
+++ header/ytl_algorithm.hpp
@@ -5,47 +5,47 @@
 
 namespace ytl
 {
-    ///Wrap the erase-remove idiom into a single call
-    /// \param container A container
-    /// \param element some element of the type inside the container
-    template <class T, typename E>
-        void erase_remove(T& container, const E& element)
-        {
-            container.erase(
-                    std::remove(
-                        std::begin(container),
-                        std::end(container),
-                        element),
-                    std::end(container));
-        }
+	///Wrap the erase-remove idiom into a single call
+	/// \param container A container
+	/// \param element some element of the type inside the container
+	template <class T, typename E>
+	void erase_remove(T& container, const E& element)
+	{
+		container.erase(
+			std::remove(
+				std::begin(container),
+				std::end(container),
+				element),
+			std::end(container));
+	}
 
-    ///Wrap the rease-remove idiom with remove_if into a single call
-    /// \param container A container
-    /// \param predicate a predicate to pass to remove_if (lambda, function poiner...)
-    template <class T, typename P>
-        void erase_remove_if(T& container, P predicate)
-        {
+	///Wrap the rease-remove idiom with remove_if into a single call
+	/// \param container A container
+	/// \param predicate a predicate to pass to remove_if (lambda, function poiner...)
+	template <class T, typename P>
+	void erase_remove_if(T& container, P predicate)
+	{
 
-            container.erase(
-                    std::remove_if(
-                        std::begin(container),
-                        std::end(container),
-                        predicate),
-                    std::end(container));
-        }
+		container.erase(
+			std::remove_if(
+				std::begin(container),
+				std::end(container),
+				predicate),
+			std::end(container));
+	}
 
 
 #include <iostream>
-    ///Print the full content of a container as spaces separated values
-    template <class T> 
-	    void print_container(const T& container)
-    {
-	    for(const auto& i : container)
-	    {
-		    std::cout << i << " ";
-	    }
-	    std::cout << "\b\n";
-    }
+	///Print the full content of a container as spaces separated values
+	template <class T>
+	void print_container(const T& container)
+	{
+		for (const auto& i : container)
+		{
+			std::cout << i << " ";
+		}
+		std::cout << "\b\n";
+	}
 
 }
 
--- header/ytl_memory.hpp
+++ header/ytl_memory.hpp
@@ -2,23 +2,25 @@
 #define YTL_MEMORY
 
 #include <memory>
+#include <cstring>
+
 namespace ytl
 {
 	///simple non-owning pointer representation
-	template <typename T> 
-		class observer_ptr
+	template <typename T>
+	class observer_ptr
 	{
 		T* ptr_ = nullptr;
 
-		public:
-		
+	public:
+
 		using element_type = T;
 		using pointer = T*;
 		using reference = T&;
 
-		observer_ptr(std::unique_ptr<T> const& ptr) : ptr_{ptr.get()} {};
-		observer_ptr(std::shared_ptr<T> const& ptr) : ptr_{ptr.get()} {};
-		observer_ptr(T* ptr) : ptr_{ptr}{};
+		observer_ptr(std::unique_ptr<T> const& ptr) : ptr_{ ptr.get() } {};
+		observer_ptr(std::shared_ptr<T> const& ptr) : ptr_{ ptr.get() } {};
+		observer_ptr(T* ptr) : ptr_{ ptr } {};
 		observer_ptr() = default;
 		~observer_ptr() = default;
 
@@ -26,14 +28,21 @@ namespace ytl
 		observer_ptr<T>& operator=(observer_ptr<T> const&) = default;
 		observer_ptr(observer_ptr<T>&&) = default;
 		observer_ptr<T>& operator=(observer_ptr<T>&&) = default;
-		
-		T* operator->() const {return ptr_;}
-		T* get() const {return ptr_;}
-		
-		T operator*() const {return *ptr_;}
-		operator T*() const {return ptr_;}
 
+		T* operator->() const { return ptr_; }
+		T* get() const { return ptr_; }
+
+		T operator*() const { return *ptr_; }
+		operator T* () const { return ptr_; }
 	};
+
+
+	template <typename T>
+	static void zero_memory(T* ptr)
+	{
+		memset(ptr, 0, sizeof(T));
+	}
+
 }
 
 #endif
